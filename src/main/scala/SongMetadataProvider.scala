f965cdaef8b187efcf5590eb1827d3acfd1bc093
Renuka Apte <renuka@wibidata.com>
2014-04-28 17:32:39 -0700

    broken

--- src/main/scala/SongMetadataProvider.scala
+++ src/main/scala/SongMetadataProvider.scala
@@ -1,3 +1,4 @@
+import com.twitter.logging.Logger
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.kiji.express.flow.util.ResourceUtil._
 import org.kiji.schema.layout.KijiTableLayout
@@ -5,27 +6,37 @@ import org.kiji.schema.{KijiTableReaderPool, KijiTable, KijiColumnName, KijiURI}
 import scala.util.parsing.json.JSONObject
 import scala.util.parsing.json.JSONObject
 
-class SongMetadataProvider {
-  def getSongMetadata(config: SongMetadataConfig): JSONObject = {
-    val tableURI: KijiURI = KijiURI.newBuilder(config.songTable).build()
-    val columnName: KijiColumnName = tableURI.getColumns.get(0)
-    val familyName = columnName.getFamily
-    val qualifier = columnName.getQualifier
+class SongMetadataProvider(config: SongMetadataConfig, log: Logger) {
+  var mTableURI: KijiURI = null
+  var mColumnName: KijiColumnName = null
+  var mProductsTable: KijiTable = null
+  var mReaderPool: KijiTableReaderPool = null
+  var mTableLayout: KijiTableLayout = null
+  val mLogger = log
 
-    var mProductsTable: KijiTable = null
-    var mReaderPool: KijiTableReaderPool = null
-    var mTableLayout: KijiTableLayout = null
+  // call setup at instantiation
+  setup(config)
+
+  def setup(config: SongMetadataConfig) {
+    mLogger.debug("Setup SongMetadataProvider")
+
+    mTableURI = KijiURI.newBuilder(config.songTable).build()
+    mColumnName = mTableURI.getColumns.get(0)
 
     val conf = HBaseConfiguration.create()
-    withKijiTable(tableURI, conf) {
+    withKijiTable(mTableURI, conf) {
       table: KijiTable => {
         mProductsTable = table
         mReaderPool = KijiTableReaderPool.Builder.create()
-          .withExhaustedAction(KijiTableReaderPool.Builder.WhenExhaustedAction.GROW)
-          .withReaderFactory(table.getReaderFactory)
-          .build()
+            .withExhaustedAction(KijiTableReaderPool.Builder.WhenExhaustedAction.GROW)
+            .withReaderFactory(table.getReaderFactory)
+            .build()
         mTableLayout = table.getLayout
       }
     }
+  }
+
+  def getSongMetadata(songName: String): String = {
+    "Loading from " + mTableURI.getTable
   }
 }
